b1c6ef92dbc8300907e22d2df1f78f20def5e7a6
YunChuan Wang <33156674+supermanwasd@users.noreply.github.com>
2022-06-19 10:47:57 +0800

    merge cufflik fpkm and Sort sample name
    merge cufflik fpkm and Sort sample name

---
+++ merge_cufflik_FPKM.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+df_0 = pd.read_table("assembly/"+os.listdir("assembly/")[0]+"/genes.fpkm_tracking")
+df_0 = df_0[["gene_short_name","FPKM"]]
+df_0.columns = ["gene_id",os.listdir("assembly/")[0]+"_FPKM"]
+
+for i in os.listdir("assembly/")[1:]:
+    df = pd.read_table("assembly/"+i+"/genes.fpkm_tracking")
+    df = df[["gene_short_name","FPKM"]]
+    df.columns = ["gene_id",i+"_FPKM"]
+    df_0 = df_0.merge(df,on="gene_id")
+
+
+columns_list = list(df_0.columns)[1::]
+columns_list.sort()
+df_0 = df_0[["gene_id"]+columns_list]
+df_0.to_csv("FPKM_list.csv",index= False)
